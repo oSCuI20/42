2f50cd5e0a20d21f1ae51362a2a48ecd60c6f67e
Eduardo Banderas <oscui20@gmail.com>
2023-02-05 17:22:08 +0100

    born2beroot

---
+++ born2beroot/auto-install.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+#
+# Installation packages in chroot environment
+#
+
+_FIREWALL_POLICY=("DROP" "DROP" "DROP")
+_TCP_PORTS=("80" "443" "4242")
+_UDP_PORTS=
+
+main() {
+    [ -z "${_DISKMAP}" ] && {
+        echo "$0 failed"
+    }
+
+    _adduser
+
+    _set_locales
+    _apt_upgrade
+
+    _apt_install \
+        linux-image-amd64 \
+        ca-certificates \
+        net-tools \
+        ntp \
+        openssh-server \
+        binutils \
+        isc-dhcp-client \
+        apparmor-utils \
+        iptables \
+        iptables-persistent \
+        sudo \
+        lvm2 \
+        cryptsetup
+    
+    _set_crypttab
+    _grub_install 
+    
+    _set_firewall
+}
+
+_set_locales() {
+    _apt_install locales
+    sed -i -s 's/# es_ES.UTF-8 UTF-8/es_ES.UTF-8 UTF-8/g' /etc/locale.gen
+    locale-gen
+}
+
+_set_crypttab() {
+    local _uuid="$(blkid /dev/mapper/${_DISKMAP##*/}p5 | grep -Eo  'UUID=(\"[0-9a-fA-F-]+\") ')"
+    echo "${_CRYPTMAP}  ${_uuid} none luks,discard" >> /etc/crypttab
+}
+
+_adduser() {
+    [ -z "${_ADDUSER}" ] && {
+        echo "can not add a user, global _ADDUSER variable is empty"
+        return 1
+    }
+
+    useradd -s /bin/bash -d /home/${_ADDUSER} -G sudo ${_ADDUSER}
+    yes ${_ADDUSER}-password | passwd ${_ADDUSER}
+}
+
+_apt_upgrade() {
+    apt update -y
+    apt upgrade -y
+}
+
+_apt_install() {
+    apt update -y
+    apt install -y ${@}
+}
+
+_systemd_enabled() {
+    systemctl enable ${@}
+}
+
+_grub_install() {
+    _apt_install grub-common grub2-common grub-pc grub-pc-bin
+    grub-install --boot-directory /boot ${_DISKMAP}
+    mv /etc/grub.d/30_os-prober /root/.
+    update-initramfs -u
+    grub-mkconfig --output /boot/grub/grub.cfg
+}
+
+_set_firewall() {
+    _set_iptables_flush
+    _set_iptables_default_policy
+    _set_iptables_input 
+    _set_iptables_output
+
+    iptables-save > /etc/iptables/rules.v4
+}
+
+_set_iptables_flush() {
+    # Flush rules
+    /sbin/iptables -F 
+    /sbin/iptables -X
+    /sbin/iptables -Z 
+
+    # Flush nat rules
+    /sbin/iptables -t nat -F
+    /sbin/iptables -t nat -X
+    /sbin/iptables -t nat -Z
+
+    /sbin/iptables -t mangle -F
+    /sbin/iptables -t mangle -X
+    /sbin/iptables -t mangle -Z
+
+    /sbin/iptables -t filter -F
+    /sbin/iptables -t filter -X
+    /sbin/iptables -t filter -Z    
+}
+
+_set_iptables_default_policy() {
+    local _default_input=${_FIREWALL_POLICY[0]}
+    local _default_output=${_FIREWALL_POLICY[1]}
+    local _default_forward=${_FIREWALL_POLICY[2]}
+
+    [ "${_default_input}"   == "" ] && local _default_input="DROP"
+    [ "${_default_output}"  == "" ] && local _default_output="DROP"
+    [ "${_default_forward}" == "" ] && local _default_forward="DROP"
+
+    /sbin/iptables -P INPUT ${_default_input}
+    /sbin/iptables -P OUTPUT ${_default_output}
+    /sbin/iptables -P FORWARD ${_default_forward}
+
+    # General filter for loopback
+    /sbin/iptables -A INPUT -i lo -j ACCEPT
+    /sbin/iptables -A OUTPUT -o lo -j ACCEPT
+}
+
+_set_iptables_input() {
+    /sbin/iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+    for _port in ${_TCP_PORTS[@]}; do 
+        /sbin/iptables -A INPUT -p tcp --dport ${_port} -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+    done
+}
+
+_set_iptables_output() {
+    /sbin/iptables -A OUTPUT -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+}
+main
--- born2beroot/install.sh
+++ born2beroot/install.sh
@@ -1,219 +1,392 @@
 #!/bin/bash
 #
-# description: Configure system with requirements
-# secretkey: 12341234
+# Install debian into img file
+#
+_HOSTNAME="edbander42"
 
-MYUSER=edbander
+_ROOT_DIRECTORY=
+_MOUNT_PATH=
+_USERNAME=
+_PUBKEY=
+_LOOP=
 
-# run as root
-[ "$(id -u)" != "0" ] && {
-  echo "Run script as root"
-  exit
+_DEBIAN_VERSION="bullseye"
+_DEBIAN_ARCH="amd64"
+_DEBIAN_URL="https://deb.debian.org/debian"
+
+_IMG_FILENAME="./disk-${_DEBIAN_VERSION}-${_DEBIAN_ARCH}.img"
+_IMG_SIZE="25G"
+_PARTED_SCRIPT="
+mklabel msdos
+mkpart primary      fat32    1MiB        510MiB
+mkpart extended              511MiB      100%
+mkpart logical      ext4     512Mib      100%
+set 1 boot on
+"
+_INITRAMFS=false
+
+_CRYPT_NAME="lvm-crypto"
+_LVM_GROUP="vg-crypt"
+_LVM_LOGICAL_VOLUME=(
+    "-L 10G -n lv--root ${_LVM_GROUP}"
+    "-L 1G  -n lv--swap ${_LVM_GROUP}"
+    "-L 3G  -n lv--home ${_LVM_GROUP}"
+    "-L 3G  -n lv--var ${_LVM_GROUP}"
+    "-L 1G  -n lv--srv ${_LVM_GROUP}"
+    "-L 1G  -n lv--tmp ${_LVM_GROUP}"
+    "-L 3G  -n lv--var-log ${_LVM_GROUP}"
+)
+
+main() {
+    [ $(id -u) -ne 0 ] && {
+        echo "this script should be run with root user"
+        exit 1
+    }
+
+    parse_arguments "$@"
+
+    _install
+    _qemu_create_image || {
+        echo "error build image"
+        return 1
+    }
+    _build_filesystem || {
+        echo "error build filesystem"
+        return 1
+    }
+
+    _debootstrap
+
+    _mount "--make-rslave --bind" /sys rootfs/sys
+    _mount "--make-rslave --bind" /dev rootfs/dev
+    _mount "-t proc" /proc rootfs/proc
+
+    _provisioning
+    _chroot_clean_image
+    _umount
+
+    # convert raw image to qcow format
+    qemu-img convert ${_IMG_FILENAME} ${_IMG_FILENAME%.*}.qcow -O qcow
 }
-apt update -y
-apt upgrade -y
 
-groupadd user42
-usermod -aG user42 ${MYUSER}
+_install() {  # requirements into docker file
+    apt update -y
+    apt install -y binfmt-support debootstrap qemu qemu-user-static qemu-system-arm qemu-utils parted kpartx lvm2 cryptsetup
+}
 
-# install sudo and config
-apt install sudo
+_provisioning() {
+    # fix permissions /tmp
+    chmod 1777 ${_MOUNT_PATH}/rootfs/tmp
+    cp ${_ROOT_DIRECTORY}/auto-install.sh ${_MOUNT_PATH}/rootfs/tmp/auto-install.sh
+    chmod +x ${_MOUNT_PATH}/rootfs/tmp/auto-install.sh
 
-echo "
-Warning!
-You are use sudo command!
-" > /etc/sudoers.d/.message
+    _ADDUSER=${_USERNAME} _DISKMAP=${_LOOP} _CRYPTMAP=${_CRYPT_NAME} chroot ${_MOUNT_PATH}/rootfs /tmp/auto-install.sh
 
-echo "
-#
-# Default configuration for sudoers
-#
-Defaults      env_reset
-Defaults      mail_badpass
-Defaults      secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-Defaults      log_host, log_year, logfile="/var/log/sudo.log"
-Defaults      lecture_file="/etc/sudoers.d/message", lecture="always"
-Defaults      badpass_message="bad password"
-Defaults      passwd_tries=3
-Defaults      insults
-Defaults      log_input,log_output
-Defaults      requiretty
+    cp -R ${_ROOT_DIRECTORY}/rootfs/* ${_MOUNT_PATH}/rootfs/.
 
-@includedir /etc/sudoers.d
-" > /etc/sudoers
+    echo "{_USERNAME}   ALL=(ALL) NOPASSWD: /usr/local/bin/monitoring.sh" >> ${_MOUNT_PATH}/rootfs/etc/sudoers.d/00-allow-command
+    _set_hostname
+    _set_hosts
+    _set_root_authentication
+    _set_fstab
+    _set_apt_configuration
+    ${_INITRAMFS} && {
+        mkdir -p ${_MOUNT_PATH}/rootfs/usr/share/initramfs-tools
+        cp -r $(pwd)/initramfs/* ${_MOUNT_PATH}/rootfs/usr/share/initramfs-tools/.
+    }
+}
 
-echo "
-#
-# Alias specification
-#
-" > /etc/sudoers.d/00-aliases
+_chroot_clean_image() {
+    [ -n "${_MOUNT_PATH}" ] || {
+        echo "err _MOUNT_PATH variable is null"
+        exit
+    }
+    find ${_MOUNT_PATH}/var/lib/apt/lists -type f | xargs rm -f
 
-echo "
-#
-# Allow members of groups
-#
-%sudo     ALL=(ALL:ALL) ALL
-" > /etc/sudoers.d/00-allow-members-groups
+    rm -rf ${_MOUNT_PATH}/var/log/journal/*
 
-echo "
-#
-# Allow command or script without password
-#
-${MYUSER}   ALL=(ALL) NOPASSWD: /usr/local/bin/monitoring.sh
-" > /etc/sudoers.d/00-allow-command
+    rm -f ${_MOUNT_PATH}/var/cache/apt/*.bin
+    rm -f ${_MOUNT_PATH}/var/log/*.log
+    rm -f ${_MOUNT_PATH}/var/log/apt/*.log
+    rm -f ${_MOUNT_PATH}/var/log/btmp
+    rm -f ${_MOUNT_PATH}/var/log/dmesg
+    rm -f ${_MOUNT_PATH}/var/log/faillog
+    rm -f ${_MOUNT_PATH}/var/log/fsck/*
+    rm -f ${_MOUNT_PATH}/var/log/lastlog
+    rm -f ${_MOUNT_PATH}/var/log/wtmp
+}
 
-usermod -aG sudo ${MYUSER}
-# end sudo install and configuration
+_qemu_create_image() {
+    [ -f ${_IMG_FILENAME} ] && {
+        echo "${_IMG_FILENAME} exists, do you want remote it?"
+        echo "press enter for remove old file or CTRL+C for cancel"
+        read
+        _umount
+        rm -Rf ${_IMG_FILENAME}
+    }
+    qemu-img create ${_IMG_FILENAME} ${_IMG_SIZE} || return 1  # stop scripts if image creation fails
+}
 
-# strong passwords
-apt install libpam-pwquality
+_build_filesystem() {
+    echo "Building filesystem"
+    [ -f ${_IMG_FILENAME} ] || {
+        echo "_build_filesystem '${_IMG_FILENAME}' not found"
+        return 1
+    }
+    parted --script --align optimal ${_IMG_FILENAME} -- ${_PARTED_SCRIPT}
 
-echo "
-#
-# /etc/pam.d/common-password - password-related modules common to all services
-#
-password  requisite                   pam_pwquality.so retry=3 minlen=10 ucredit=-1 dcredit=-1 maxrepeat=3 reject_username difok=7 enforce_for_root
-password	[success=1 default=ignore]	pam_unix.so obscure use_authtok try_first_pass yescrypt
-password	requisite			pam_deny.so
-password	required			pam_permit.so
-password	optional	pam_gnome_keyring.so
-" > /etc/pam.d/common-password
+    echo "Loading loop disk"
+    _LOOP=$(kpartx ${_IMG_FILENAME} | head -1 | awk -F' ' '{print $5}')
+    kpartx -av ${_IMG_FILENAME} 
 
-echo "
-MAIL_DIR        /var/mail
-FAILLOG_ENAB		yes
-LOG_UNKFAIL_ENAB	no
-LOG_OK_LOGINS		no
+    [ -z "${_LOOP}" ] && {
+        echo "failed, _LOOP kpartx can not get loop devices"
+        return 1
+    }
+    
+    mkfs.vfat -F 32 -n boot /dev/mapper/${_LOOP##*/}p1 || return 1
 
-SYSLOG_SU_ENAB		yes
-SYSLOG_SG_ENAB		yes
+    cryptsetup luksFormat --type luks1 /dev/mapper/${_LOOP##*/}p5
+    echo "DECRYPTED VOLUME /dev/mapper/${_LOOP##*/}p5"
+    cryptsetup luksOpen /dev/mapper/${_LOOP##*/}p5 ${_CRYPT_NAME}
 
-FTMP_FILE	/var/log/btmp
+    {
+        pvcreate /dev/mapper/${_CRYPT_NAME} || {
+            echo "can not create a physical volumes"
+            return 1
+        }
 
-SU_NAME		su
+        vgcreate "vg-crypt" /dev/mapper/${_CRYPT_NAME} || {
+            echo "can not create a virtual volumes"
+            return 1
+        }
 
-HUSHLOGIN_FILE	.hushlogin
+        for n in $(seq 0  $((${#_LVM_LOGICAL_VOLUME[@]} - 1))); do 
+            local lvm_volume=$(echo ${_LVM_LOGICAL_VOLUME[$n]} | awk -F' ' '{print $4}')
+            local _tag=${lvm_volume##*--}
+            local _device="/dev/${_LVM_GROUP}/${lvm_volume}"
+            lvcreate ${_LVM_LOGICAL_VOLUME[$n]}
 
-ENV_SUPATH	PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-ENV_PATH	PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
+            [ "${_tag}" == "swap" ] && {
+                mkswap ${_device}
+                continue 
+            }
+            mkfs.ext4 -F -L "${_tag}fs" ${_device} || return 1
+            [ "${_tag}" == "root" ] && {
+                _rootfs="${_tag}fs"
+                _mount "-t ext4" ${_device} ${_rootfs} || return 1
+                continue
+            }
 
-TTYGROUP	tty
-TTYPERM		0600
+            _mount "-t ext4" ${_device} ${_rootfs}/${_tag/-/\/} || return 1
+        done 
+    }
 
-ERASECHAR	0177
-KILLCHAR	025
-UMASK		022
+    [ $? -ne 0 ] && { 
+        echo "can not create lvm volumes"
+        return 1
+    }
 
-PASS_MAX_DAYS	30
-PASS_MIN_DAYS	2
-PASS_WARN_AGE	7
+    _mount "-t vfat" /dev/mapper/${_LOOP##*/}p1 rootfs/boot || return 1
 
-UID_MIN			 1000
-UID_MAX			60000
+    return 0
+}
 
-GID_MIN			 1000
-GID_MAX			60000
+_mount() {
+    local options="${1}"
+    local disk="${2}"
+    local path="${_MOUNT_PATH}/${3}"
 
-LOGIN_RETRIES		5
-LOGIN_TIMEOUT		60
+    mkdir -vp ${path}
 
-CHFN_RESTRICT		rwh
+    mount ${options} ${disk} ${path}
+    
+    return $?
+}
 
-DEFAULT_HOME	yes
-USERGROUPS_ENAB yes
-ENCRYPT_METHOD SHA512
-" > /etc/login.defs
-# end strong passwords
+_umount() {
+    sync
+    umount "${_MOUNT_PATH}/rootfs/dev"
+    umount "${_MOUNT_PATH}/rootfs/proc"
+    umount "${_MOUNT_PATH}/rootfs/sys"
+    umount -R "${_MOUNT_PATH}/rootfs"
 
-# ssh services
-echo "
-Include /etc/ssh/sshd_config.d/*.conf
+    for n in $(seq 0  $((${#_LVM_LOGICAL_VOLUME[@]} - 1))); do 
+        local lvm_volume=$(echo ${_LVM_LOGICAL_VOLUME[$n]} | awk -F' ' '{print $4}')
+        lvchange -an /dev/${_LVM_GROUP}/${lvm_volume}
+    done 
 
-Port 4242
-#AddressFamily any
-ListenAddress 0.0.0.0
-#ListenAddress ::
+    cryptsetup luksClose /dev/mapper/${_CRYPT_NAME}
 
-#HostKey /etc/ssh/ssh_host_rsa_key
-#HostKey /etc/ssh/ssh_host_ecdsa_key
-#HostKey /etc/ssh/ssh_host_ed25519_key
+    kpartx -d ${_IMG_FILENAME}
+}
 
-# Ciphers and keying
-#RekeyLimit default none
+_debootstrap() {
+    debootstrap \
+        --arch=${_DEBIAN_ARCH} \
+        --foreign \
+        ${_DEBIAN_VERSION} \
+        ${_MOUNT_PATH}/rootfs \
+        ${_DEBIAN_URL}
 
-# Logging
-SyslogFacility AUTH
-LogLevel INFO
+    mkdir -p ${_MOUNT_PATH}/rootfs/usr/bin
+    cp /usr/bin/qemu-arm-static ${_MOUNT_PATH}/rootfs/usr/bin/.
+    cat << EOF > ${_MOUNT_PATH}/rootfs/debootstrap/mirror
+${_DEBIAN_URL}
+EOF
+    chroot ${_MOUNT_PATH}/rootfs /debootstrap/debootstrap --second-stage
+}
 
-# Authentication:
+_set_apt_configuration() {
+    mkdir -vp ${_MOUNT_PATH}/rootfs/etc/apt/trusted.gpg.d
+    cp -v /etc/apt/trusted.gpg.d/debian-archive*.gpg ${_MOUNT_PATH}/rootfs/etc/apt/trusted.gpg.d/.
+    cat << EOF > ${_MOUNT_PATH}/rootfs/etc/apt/sources.list
+deb http://deb.debian.org/debian/ ${_DEBIAN_VERSION} main contrib
+deb http://security.debian.org/debian-security ${_DEBIAN_VERSION}-security main contrib
+deb http://deb.debian.org/debian/ ${_DEBIAN_VERSION}-updates main contrib
+EOF
+}
 
-LoginGraceTime 2m
-PermitRootLogin no
-StrictModes yes
-MaxAuthTries 6
-MaxSessions 10
+_set_hostname() {
+    cat << EOF > ${_MOUNT_PATH}/rootfs/etc/hostname
+${_HOSTNAME}
+EOF
+}
 
-#PubkeyAuthentication yes
+_set_hosts() {
+    cat << EOF > ${_MOUNT_PATH}/rootfs/etc/hosts
+127.0.0.1 localhost ${_HOSTNAME}
 
-# Expect .ssh/authorized_keys2 to be disregarded by default in future.
-#AuthorizedKeysFile	.ssh/authorized_keys .ssh/authorized_keys2
+::1     ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+ff02::3 ip6-allhosts
+EOF
+}
 
-#AuthorizedPrincipalsFile none
+_set_fstab() {
+    local _fstab="# <file system>\t\t\t<dir>\t\t\ttype\t\t\t<options>\t\t\t0\t\t\t0\n"
+    for n in $(seq 0  $((${#_LVM_LOGICAL_VOLUME[@]} - 1))); do 
+        local lvm_volume=$(echo ${_LVM_LOGICAL_VOLUME[$n]} | awk -F' ' '{print $4}')
+        local _tag=${lvm_volume##*--}
+        local _device="/dev/${_LVM_GROUP}/${lvm_volume}"
+        local _uuid=$(blkid ${_device} | grep -Eo  'UUID=(\"[0-9a-fA-F-]+\") ')
 
-#AuthorizedKeysCommand none
-#AuthorizedKeysCommandUser nobody
+        [ "${_tag}" == "swap" ] && {
+            local _fstab="${_fstab}${_uuid}\t\t\tnone\t\t\tswap\t\t\tsw\t\t\t0\t\t\t0\n"
+            continue 
+        }
+        
+        [ "${_tag}" == "root" ] && {
+            local _fstab="${_fstab}${_uuid}\t\t\t/\t\t\text4\t\t\terrors=remount-ro\t\t\t0\t\t\t1\n"
+            continue
+        }
 
-# To disable tunneled clear text passwords, change to no here!
-PasswordAuthentication yes
-PermitEmptyPasswords no
+        local _fstab="${_fstab}${_uuid}\t\t\t/${_tag/-/\/}\t\t\text4\t\t\tdefaults\t\t\t0\t\t\t2\n"
+    done 
 
-# Change to yes to enable challenge-response passwords (beware issues with
-# some PAM modules and threads)
-ChallengeResponseAuthentication no
+    local _uuid="$(blkid /dev/mapper/${_LOOP##*/}p1 | grep -Eo  'UUID=(\"[0-9a-fA-F-]+\") ')"
+    local _fstab="${_fstab}${_uuid}\t\t\t/boot\t\t\tvfat\t\t\tumask=0077\t\t\t0\t\t\t1\n"
 
-UsePAM yes
+    echo -e "${_fstab}" > ${_MOUNT_PATH}/rootfs/etc/fstab
+}
 
-AllowAgentForwarding no
-AllowTcpForwarding no
-X11Forwarding no
-PrintMotd no
-
-# no default banner path
-#Banner none
-
-# Allow client to pass locale environment variables
-AcceptEnv LANG LC_*
-
-# override default of no subsystems
-Subsystem	sftp	/usr/lib/openssh/sftp-server
-
-# Example of overriding settings on a per-user basis
-#Match User anoncvs
-#	X11Forwarding no
-#	AllowTcpForwarding no
-#	PermitTTY no
-#	ForceCommand cvs server
-" > /etc/ssh/sshd_config
-
-# install apparmor
-apt install apparmor apparmor-utils apparmor-profiles apparmor-profiles-extra
-
-# Enable apparmor (debian)
-mkdir -p /etc/default/grub.d/
-echo 'GRUB_CMDLINE_LINUX_DEFAULT="$GRUB_CMDLINE_LINUX_DEFAULT apparmor=1 security=apparmor"' \
-  | tee /etc/default/grub.d/apparmor.cfg
-update-grub
-echo "Y" > /sys/module/apparmor/parameters/enabled
-
-aa-status
-
-echo "*/10 * * * * /usr/local/bin/monitoring.sh" >> /var/spool/cron/crontabs/root
+_set_root_authentication() {
+    mkdir -p ${_MOUNT_PATH}/rootfs/root/.ssh
+    chown root:root ${_MOUNT_PATH}/rootfs/root/.ssh
+    chmod 700 ${_MOUNT_PATH}/rootfs/root/.ssh
+    [ -f ${_PUBKEY} ] || {
+        echo "${_PUBKEY} not found"
+        return 1
+    }
+    cat ${_PUBKEY} > ${_MOUNT_PATH}/rootfs/root/.ssh/authorized_keys
+    return $?
+}
 
 
-# bonus 
-apt install lighttpd mariadb-server php php-cgi php-fpm php-mysql -y
+_usage() {
+    cat << EOF
+Options:
+    --hostname hostname, default: ${_HOSTNAME}
+    --deb-version codename of debian, default: ${_DEBIAN_VERSION}
+    --deb-arch system architecture, default: ${_DEBIAN_ARCH}
+    --deb-url URL to download packages, default: ${_DEBIAN_URL}
+    --img-filename name or filepath of the image, default: ${_IMG_FILENAME}
+    --img-size set size assign, example 8G, 128M, ..., default: ${_IMG_SIZE}
+    --username adduser to operating system, required
+    --pub-key pubkey-to-copy-into-authorized_keys, required
+    --initramfs flag to copy initramfs configurations, this is an optional argument
 
-lighty-enable-mod fastcgi
-lighty-enable-mod fastcgi-php
+Usage:
+    ${0} --pub-key path/to/file.pub --openvpn-client path/to/client.ovpn
+EOF
+}
 
-systemctl enable lighttpd php-fpm mariadb
+parse_arguments() {
+    while [ $# -ge 1 ]; do
+        key=${1/ /}
+
+        if  [ "${key}" != "--initramfs" ]; then
+            shift
+            value=$1
+        fi
+
+        case ${key} in
+            --help)
+                _usage
+                exit 0
+                ;;
+            --hostname)
+                _HOSTNAME=${value}
+                ;;
+            --username)
+                _USERNAME=${value}
+                ;;
+            --deb-version)
+                _DEBIAN_VERSION=${value}
+                ;;
+            --dev-arch)
+                _DEBIAN_ARCH=${value}
+                ;;
+            --deb-url)
+                _DEBIAN_URL=${value}
+                ;;
+            --img-filename)
+                _IMG_FILENAME=${value}
+                ;;
+            --img-size)
+                _IMG_SIZE=${value}
+                ;;
+            --pub-key)
+                _PUBKEY=${value}
+                ;;
+            --initramfs)
+                _INITRAMFS=true
+                ;;
+            *)
+                _usage
+                echo
+                echo "${key} not recognized"
+                exit 1
+                ;;
+        esac
+
+        shift
+    done
+
+    [ -z "${_PUBKEY}" ] && {
+        echo "--pub-key argument is empty"
+        exit 1
+    }
+
+    [ -z "${_USERNAME}" ] && {
+        echo "--username argument is empty"
+        exit 1
+    }
+
+    _ROOT_DIRECTORY="$(pwd)/$(dirname ${0})"
+    _MOUNT_PATH="/mnt/${_DEBIAN_VERSION}-${_DEBIAN_ARCH}"
+}
+
+main "$@"
